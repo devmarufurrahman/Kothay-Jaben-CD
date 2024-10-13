d339c98a64acc6831830749ada5f09579d1cc5b2
devmarufurrahman <mdmarufur5682@gmail.com>
2024-10-13 15:33:24 +0600

    button animator and flash overlay animator add custom

--- app/src/main/java/com/maruf/kothayjabecd/LoginActivity.kt
+++ app/src/main/java/com/maruf/kothayjabecd/LoginActivity.kt
@@ -1,22 +1,31 @@
 package com.maruf.kothayjabecd
 
+
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Context
 import android.content.Intent
+import android.health.connect.datatypes.units.Length
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import com.maruf.kothayjabecd.databinding.ActivityLoginBinding
+import com.maruf.kothayjabecd.utils.Utils
+
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var flashOverlay : View
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        flashOverlay = binding.flashOverlay
 
 
         binding.loginBtn.setOnClickListener {
@@ -24,14 +33,32 @@ class LoginActivity : AppCompatActivity() {
             val email = binding.emailInput.text.toString()
             val password = binding.passwordInput.text.toString()
             if (email.isNotEmpty() && password.isNotEmpty()) {
-                binding.progressbar.visibility = View.GONE
-                Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                startActivity(intent)
+
+                Utils.buttonAnimation(binding.loginBtn,binding.progressbar, this)
+
+                // Simulate network call with delay
+                Handler().postDelayed(Runnable {
+                    val animateSuccess = Utils.animateSuccess(binding.loginBtn,binding.progressbar,"Login Successfully", 300,this,this)
+
+                    if (animateSuccess){
+                        //Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show()
+
+
+                        Utils.flashOverlayEffect(binding.flashOverlay, binding.main) {
+                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+//                            overridePendingTransition()
+                        }
+                    }
+
+                }, 2000) // Delay to simulate network call
+
+
             } else {
                 binding.progressbar.visibility = View.GONE
                 Toast.makeText(this, "Empty Fields Are not Allowed", Toast.LENGTH_SHORT).show()
-
+                Utils.animateFailure("Login", binding.loginBtn, this)
             }
         }
 
@@ -46,4 +73,7 @@ class LoginActivity : AppCompatActivity() {
 //        }
 
     }
+
+
+
 }
---
+++ app/src/main/java/com/maruf/kothayjabecd/utils/Utils.kt
@@ -0,0 +1,123 @@
+package com.maruf.kothayjabecd.utils
+
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Handler
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.maruf.kothayjabecd.MainActivity
+import com.maruf.kothayjabecd.R
+import kotlin.time.Duration
+
+object Utils {
+    fun buttonAnimation(button: Button, progressBar: ProgressBar, context: Context) {
+
+        // Show the loading spinner and hide the button text
+        button.text = ""
+        progressBar.visibility = View.VISIBLE
+        val setPadding = 20.dpToPx(context)
+        button.setPadding(setPadding,setPadding,setPadding,setPadding)
+    }
+
+
+    fun animateSuccess(button: Button, progressBar: ProgressBar, successMsg: String, duration: Long, context: Context, activity: Activity):Boolean {
+
+        // Stop loading and apply appropriate animation
+        progressBar.visibility = View.GONE
+        val setPadding = 0.dpToPx(context)
+        button.setPadding(setPadding,setPadding,setPadding,setPadding)
+        button.text = successMsg
+        // Expanding animation
+        val expandAnimator = ObjectAnimator.ofFloat(button, "scaleX", 1f, 1.1f)
+        expandAnimator.setDuration(duration)
+        expandAnimator.start()
+
+
+        expandAnimator.addListener(object : android.animation.Animator.AnimatorListener {
+            override fun onAnimationStart(animation: android.animation.Animator) {}
+
+            override fun onAnimationEnd(animation: android.animation.Animator) {
+
+            }
+
+            override fun onAnimationCancel(animation: android.animation.Animator) {}
+
+            override fun onAnimationRepeat(animation: android.animation.Animator) {}
+        })
+        return true
+    }
+
+
+    fun animateFailure(failMsg : String, button: Button, context: Context) {
+        // Shake animation on failure
+        val shake: Animation = AnimationUtils.loadAnimation(context, R.anim.shake)
+        button.startAnimation(shake)
+
+        // Reset button text after shake animation
+        shake.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+
+            override fun onAnimationEnd(animation: Animation) {
+                button.text = failMsg
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+        })
+    }
+
+
+    fun flashOverlayEffect(flashOverlay: View, rootLayout: View, onAnimationEnd: () -> Unit){
+
+        flashOverlay.visibility = View.VISIBLE
+
+        // Animate the height of the flashOverlay from 0 to match_parent
+        val initialHeight = rootLayout.height
+        val initialWidth = rootLayout.width
+
+//      Animate from 0 to double the height and width
+        val animator = ValueAnimator.ofFloat(0f, 2f)
+        animator.duration = 400 // Flash duration, you can adjust this
+
+        animator.addUpdateListener { animation ->
+            val animatedValue = animation.animatedValue as Float
+
+            // Set the height and width of flashOverlay to double the size of the main screen
+            flashOverlay.layoutParams.height = (initialHeight * animatedValue).toInt()
+            flashOverlay.layoutParams.width = (initialWidth * animatedValue).toInt()
+
+            // Request layout to apply the updated dimensions
+            flashOverlay.requestLayout()
+        }
+        animator.start()
+
+        // Hide overlay after animation ends
+        animator.addListener(object : android.animation.Animator.AnimatorListener {
+
+            override fun onAnimationStart(animation: android.animation.Animator) {
+            }
+
+            override fun onAnimationEnd(animation: android.animation.Animator) {
+             onAnimationEnd()
+            }
+
+            override fun onAnimationCancel(animation: android.animation.Animator) {}
+
+            override fun onAnimationRepeat(animation: android.animation.Animator) {}
+        })
+
+    }
+
+
+    fun Int.dpToPx(context: Context): Int {
+        return (this * context.resources.displayMetrics.density).toInt()
+    }
+    
+    
+}
